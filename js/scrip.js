62a0d8f8ad6df2546ed432d0d943ad2d1ab5e4c4
jrbarrantesc <jrbarrantesc@est.utn.ac.cr>
2015-10-30 11:18:13 -0600

    dia 10
    se aplica framework a la pagina de registro se inteta hacer las
    valedaciones pero falla

--- js/scrip.js
+++ js/scrip.js
@@ -1,6 +1,5 @@
-   $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-     $(document).ready(function(){
-    $('.materialboxed').materialbox();
-  });
+document.col s12.addEventListener('submit', validarFormulario);
+ 
+function validarFormulario(evObject) {
+evObject.preventDefault(); //Evita el envío del formulario hasta comprobar
+}
